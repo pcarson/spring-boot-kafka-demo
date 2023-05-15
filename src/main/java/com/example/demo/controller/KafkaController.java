fb2a037a3f60c7bc5a461b270c241623dcdfe9c1
pcarson <8025daab96b77ca618a282f9a5d6ec6ddd350805>
2023-05-15 18:40:39 +0200

    CompletableFuture for logging

--- src/main/java/com/example/demo/controller/KafkaController.java
+++ src/main/java/com/example/demo/controller/KafkaController.java
@@ -7,9 +7,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Tag(name = "KafkaController", description = "Endpoint for publishing items to Kafka")
@@ -30,11 +33,23 @@ public class KafkaController {
     public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO) {
 
         try {
-            kafkaTemplate.send(topicName, userDTO);
+            // kafkaTemplate.send(topicName, userDTO);
+            sendMessageWIthCompletionLogging(userDTO);
             return ResponseEntity.accepted().build();
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
+    }
+
+    private void sendMessageWIthCompletionLogging(UserDTO userDTO) {
+        CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(topicName, userDTO);
+        future.whenComplete((result, ex) -> {
+            if (ex == null) {
+                log.info("Sent message=[{}] with offset=[{}]", userDTO, result.getRecordMetadata().offset());
+            } else {
+                log.error("Unable to send message=[" + userDTO + "] due to : " + ex.getMessage());
+            }
+        });
     }
 
 }
