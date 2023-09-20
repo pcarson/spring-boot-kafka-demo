6f43040bc4a80035337e5bb050f2f7956078a3cc
pcarson <8025daab96b77ca618a282f9a5d6ec6ddd350805>
2023-09-20 16:06:21 +0200

    sonar configuration; code recommendations

--- src/main/java/com/example/demo/configuration/KafkaConsumerConfiguration.java
+++ src/main/java/com/example/demo/configuration/KafkaConsumerConfiguration.java
@@ -9,11 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.listener.CommonErrorHandler;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.DefaultErrorHandler;
-import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
